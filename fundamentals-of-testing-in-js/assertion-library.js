e88e934ca1aa7f6efaf2eed5412e095eb3a4add4
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-26 11:53:05 +0545

    refactor: testing
    - Revamp js files to use global test functions

--- fundamentals-of-testing-in-js/assertion-library.js
+++ fundamentals-of-testing-in-js/assertion-library.js
@@ -11,23 +11,3 @@ test("Subtract", () => {
   const expected = 4;
   expect(result).toBe(expected);
 });
-
-function test(title, callback) {
-  try {
-    callback();
-    console.log(`✔️  ${title}`);
-  } catch (error) {
-    console.error(`❌ ${title}`);
-    console.error(error);
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`);
-      }
-    },
-  };
-}
--- fundamentals-of-testing-in-js/async-await.js
+++ fundamentals-of-testing-in-js/async-await.js
@@ -11,23 +11,3 @@ test("Subtract", async () => {
   const expected = 6;
   expect(result).toBe(expected);
 });
-
-async function test(title, callback) {
-  try {
-    await callback();
-    console.log(`✔️  ${title}`);
-  } catch (error) {
-    console.error(`❌ ${title}`);
-    console.error(error);
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`);
-      }
-    },
-  };
-}
